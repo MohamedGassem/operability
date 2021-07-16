6df3d1a0599e40480dc10cf5f222477ff0b04107
Mohamed Gassem <78160905+MohamedGassem@users.noreply.github.com>
2021-07-16 12:51:56 +0200

    bug fix

--- operability.py
+++ operability.py
@@ -30,8 +30,8 @@ st.write('Avec les paramètres à gauche, tu peux ajouter autant de tâches que 
 with st.sidebar:
     st.title('Paramètres')
     nb_taches = st.number_input('Entrez le nombre de tâches', value=3)
-    task_time = np.empty(shape=nb_taches)
-    task_list = np.empty(shape=nb_taches)
+    task_time = []
+    task_list = []
     time_spent = st.number_input('Entrez le temps de travail total (en heures)', value=8)
     total_hour = time_spent * 60
     if not time_spent:
@@ -39,16 +39,16 @@ with st.sidebar:
     #if new_task:
     operateur = st.text_input('Nom de l\'opérateur')
     if not operateur:
-        st.warning('Entrez un nom ci-dessus')
-        
+        st.warning('Entrez un temps de travail ci-dessus')
+
 task_lists = ['Bennes jaunes', 'Bennes vertes', 'Amorçage', 'Autocontrôle', 'Partiels']
 for i in range(nb_taches):
-    option = st.selectbox(f'Tâche {i+1}', task_lists, key=i)
+    option = st.selectbox(f'Test {i+1}', task_lists, key=i)
     h1,h2,h3,h4 = st.beta_columns(4)
     time = h1.number_input('Temps passé sur cette tâche', value=10, key=i)
     frequency = h2.number_input('Nombre de fois que cette tâche a été faite', value=1, key=1000*i)
-    task_time[i] = time * frequency
-    task_list[i] = option
+    task_time.append(time * frequency)
+    task_list.append(option)
     st.write(task_time[i])
 if st.button('calculer'):
     task_dict = operability(task_time, task_list, total_hour)
